64789d23bc6ce71b54f3e638e38460a970ae74e7
loganrun <gtbrli2000>
2018-01-01 12:43:07 -0800

    rvideo

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ $(document).ready(() => {
     'https://www.googleapis.com/youtube/v3/search',{
       part: 'snippet',
       key:  'AIzaSyDaP4y3Gi9tfwIz7wWKE_E3f6hH5GtCcFg',
-      q:  searchText },
+      q:  searchText,
+      maxResults: 12 },
+
     function(data){
       $.each(data.items, function(i, item){
         let vidId = item.id.videoId;
@@ -24,18 +26,10 @@ $(document).ready(() => {
   function renderVideo(vidId,vidTitle){
     $('.results').append(`
       <div class="col s12 l3">
-          <div class="card">
-            <div class="card-image">
-            <iframe src=\"//www.youtube.com/embed/${vidId}"><iframe>
+      <div class="video-container">
+        <iframe width="853" height="480" src=\"//www.youtube.com/embed/${vidId}" frameborder="0" allowfullscreen></iframe>
+      </div>
 
-            </div>
-            <div class="card-content">
-              <p> ${vidTitle} </p>
-            </div>
-            <div class="card-action">
-              <a href="#">This is a link</a>
-            </div>
-          </div>
       </div>
       `)
 
@@ -49,3 +43,16 @@ $(document).ready(() => {
 
 
 });
+
+// <div class="card">
+//   <div class="card-image">
+//   <iframe src=\"//www.youtube.com/embed/${vidId}"><iframe>
+//
+//   </div>
+//   <div class="card-content">
+//     <p> ${vidTitle} </p>
+//   </div>
+//   <div class="card-action">
+//     <a href="#">This is a link</a>
+//   </div>
+// </div>
