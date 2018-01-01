fd42b936f196a5453f3f5e3f9f79c1f5adc221a5
loganrun <gtbrli2000>
2018-01-01 11:29:03 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+$(document).ready(() => {
+  $('#searchForm').on('submit', (e) =>{
+    let searchText = $('#search').val();
+    getVideos(searchText);
+    e.preventDefault();
+  });
+
+  function getVideos(searchText){
+  $.get(
+    'https://www.googleapis.com/youtube/v3/search',{
+      part: 'snippet',
+      key:  'AIzaSyDaP4y3Gi9tfwIz7wWKE_E3f6hH5GtCcFg',
+      q:  searchText },
+    function(data){
+      $.each(data.items, function(i, item){
+        let vidId = item.id.videoId;
+        let vidTitle = item.snippet.title;
+        renderVideo(vidId, vidTitle);
+      })
+    }
+  );
+  }
+
+  function renderVideo(vidId,vidTitle){
+    $('.results').append(`
+      <div class="col s12 l3">
+          <div class="card">
+            <div class="card-image">
+            <iframe src=\"//www.youtube.com/embed/${vidId}"><iframe>
+
+            </div>
+            <div class="card-content">
+              <p> ${vidTitle} </p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+      </div>
+      `)
+
+  }
+
+
+
+
+
+
+
+
+});
